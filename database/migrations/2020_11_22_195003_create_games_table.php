0691f56c4fe5595bf019097da307b51452961b01
Kai Philipp <kphilipp@inquies.de>
2020-11-22 23:15:39 +0100

    missing files

---
+++ database/migrations/2020_11_22_195003_create_games_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('pos1');
+            $table->bigInteger('pos2');
+            $table->bigInteger('pos3');
+            $table->bigInteger('pos4');
+            $table->bigInteger('pos5');
+            $table->bigInteger('pos6');
+            $table->bigInteger('pos7');
+            $table->bigInteger('pos8');
+            $table->bigInteger('pos9');
+            $table->bigInteger('pos10');
+            $table->datetime('game_opened');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
