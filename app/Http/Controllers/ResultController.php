1e23be9f4e1204cbb858fdc634e7741f367659e2
Kai Philipp <kphilipp@inquies.de>
2020-12-11 16:28:32 +0100

    api route results, results query raw

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Game;
 
 class ResultController extends Controller
 {
@@ -12,17 +14,38 @@ class ResultController extends Controller
     }
 
     public function index(Request $request){
-        $results = [
-            [
-                "id" => 1,
-                "type" => "regular",
-                "number" => 12234,
-                "pos1" => "winner nickname",
-                "pos2" => "2nd winner nick",
-            ]
-        ];
+        $results = DB::table('games')
+                ->leftJoin('players as p1', 'games.pos1', '=', 'p1.id')
+                ->leftJoin('players as p2', 'games.pos2', '=', 'p2.id')
+                ->leftJoin('players as p3', 'games.pos3', '=', 'p3.id')
+                ->leftJoin('players as p4', 'games.pos4', '=', 'p4.id')
+                ->leftJoin('players as p5', 'games.pos5', '=', 'p5.id')
+                ->leftJoin('players as p6', 'games.pos6', '=', 'p6.id')
+                ->leftJoin('players as p7', 'games.pos7', '=', 'p7.id')
+                ->leftJoin('players as p8', 'games.pos8', '=', 'p8.id')
+                ->leftJoin('players as p9', 'games.pos9', '=', 'p9.id')
+                ->leftJoin('players as p10', 'games.pos10', '=', 'p10.id')
+                ->select(
+                    'games.id', 'games.type', 'games.number', 'games.created_at',
+                    'p1.nickname as p1_name',
+                    'p2.nickname as p2_name',
+                    'p3.nickname as p3_name',
+                    'p4.nickname as p4_name',
+                    'p5.nickname as p5_name',
+                    'p6.nickname as p6_name',
+                    'p7.nickname as p7_name',
+                    'p8.nickname as p8_name',
+                    'p9.nickname as p9_name',
+                    'p10.nickname as p10_name'
+                )
+                ->get();
+
         return view('results', [
             "results" => $results
         ]);
+    }
+
+    public function filter(Request $request){
+        dd('Filter');
     }
 }
--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+    
 }
