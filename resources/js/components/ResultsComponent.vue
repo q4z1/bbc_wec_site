bd8499f3fc27a523d410966c12e69de00274588c
Bojan <bojan.barisic051@gmail.com>
2020-12-15 20:36:57 +0100

    Year month results buttons

--- resources/js/components/ResultsComponent.vue
+++ resources/js/components/ResultsComponent.vue
@@ -1,20 +1,53 @@
 <template>
     <div>
-        <h1>Results ?</h1>
+        <b-dropdown id="dropdown-1" text="Year" class="m-md-2">
+            <b-dropdown-item>2020</b-dropdown-item>
+            <b-dropdown-item>2019</b-dropdown-item>
+            <b-dropdown-item>2018</b-dropdown-item>
+            <b-dropdown-item>2017</b-dropdown-item>
+            <b-dropdown-item>2016</b-dropdown-item>
+            <b-dropdown-item>2015</b-dropdown-item>
+            <b-dropdown-item>2014</b-dropdown-item>
+            <b-dropdown-item>2013</b-dropdown-item>
+            <b-dropdown-item>2012</b-dropdown-item>
+            <b-dropdown-item>2011</b-dropdown-item>
+            <b-dropdown-item>2010</b-dropdown-item>
+        </b-dropdown>
+        <b-dropdown id="dropdown-1" text="Month" class="m-md-2">
+            <b-dropdown-item>January</b-dropdown-item>
+            <b-dropdown-item>February</b-dropdown-item>
+            <b-dropdown-item>March</b-dropdown-item>
+            <b-dropdown-item>April</b-dropdown-item>
+            <b-dropdown-item>May</b-dropdown-item>
+            <b-dropdown-item>June</b-dropdown-item>
+            <b-dropdown-item>July</b-dropdown-item>
+            <b-dropdown-item>August</b-dropdown-item>
+            <b-dropdown-item>September</b-dropdown-item>
+            <b-dropdown-item>October</b-dropdown-item>
+            <b-dropdown-item>November</b-dropdown-item>
+            <b-dropdown-item>December</b-dropdown-item>
+        </b-dropdown>
         <b-table striped hover 
-            :items="results"
+            :items="result"
             @row-clicked="showGame"></b-table>
+            
     </div>
 </template>
 <script>
     export default {
         props: ['results'],
+        data() {
+            return {
+                result: null,
+            }
+        },
         mounted() {
             console.log('Results mounted.')
             console.log(this.results)
 
 
             // ajax call => result.data into this.results
+            this.result = this.results
         },
         methods:{
             showGame(item, index, event) {
