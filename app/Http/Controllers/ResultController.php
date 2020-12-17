1f736b430bff5142eb4395b0ebc27f7113ff335c
Kai Philipp <kphilipp@inquies.de>
2020-12-17 18:09:42 +0100

    halloffame basic list

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -78,60 +78,16 @@ class ResultController extends Controller
 
 
     public function halloffame(Request $request){
-        $totals = DB::table('games')
-        ->leftJoin('players as p1', 'games.pos1', '=', 'p1.id')
-        ->leftJoin('players as p2', 'games.pos2', '=', 'p2.id')
-        ->leftJoin('players as p3', 'games.pos3', '=', 'p3.id')
-        ->leftJoin('players as p4', 'games.pos4', '=', 'p4.id')
-        ->leftJoin('players as p5', 'games.pos5', '=', 'p5.id')
-        ->leftJoin('players as p6', 'games.pos6', '=', 'p6.id')
-        ->leftJoin('players as p7', 'games.pos7', '=', 'p7.id')
-        ->leftJoin('players as p8', 'games.pos8', '=', 'p8.id')
-        ->leftJoin('players as p9', 'games.pos9', '=', 'p9.id')
-        ->leftJoin('players as p10', 'games.pos10', '=', 'p10.id')
+        $totals = DB::table('players')
         ->select(
-            'games.id', 'games.type', 'games.number', 'games.started',
-            'p1.nickname as p1',
-            'p2.nickname as p2',
-            'p3.nickname as p3',
-            'p4.nickname as p4',
-            'p5.nickname as p5',
-            'p6.nickname as p6',
-            'p7.nickname as p7',
-            'p8.nickname as p8',
-            'p9.nickname as p9',
-            'p10.nickname as p10'
-        )->orderBy('started', 'DESC')
+            'players.id', 'players.nickname', 'players.avatar'
+        )
         ->count();
-        $results = DB::table('games')
-            ->leftJoin('players as p1', 'games.pos1', '=', 'p1.id')
-            ->leftJoin('players as p2', 'games.pos2', '=', 'p2.id')
-            ->leftJoin('players as p3', 'games.pos3', '=', 'p3.id')
-            ->leftJoin('players as p4', 'games.pos4', '=', 'p4.id')
-            ->leftJoin('players as p5', 'games.pos5', '=', 'p5.id')
-            ->leftJoin('players as p6', 'games.pos6', '=', 'p6.id')
-            ->leftJoin('players as p7', 'games.pos7', '=', 'p7.id')
-            ->leftJoin('players as p8', 'games.pos8', '=', 'p8.id')
-            ->leftJoin('players as p9', 'games.pos9', '=', 'p9.id')
-            ->leftJoin('players as p10', 'games.pos10', '=', 'p10.id')
-            ->select(
-                'games.id', 'games.type', 'games.number', 'games.started',
-                'p1.nickname as p1',
-                'p2.nickname as p2',
-                'p3.nickname as p3',
-                'p4.nickname as p4',
-                'p5.nickname as p5',
-                'p6.nickname as p6',
-                'p7.nickname as p7',
-                'p8.nickname as p8',
-                'p9.nickname as p9',
-                'p10.nickname as p10'
-            )->orderBy('started', 'DESC')
-            ->limit(10)
-            ->whereYear('started','=', date("Y"))
-            ->whereMonth('started','=', date("m"))
-            ->where('type', 1) // default type
-            ->get();
+        $results = DB::table('players')
+        ->select(
+            'players.id', 'players.nickname', 'players.avatar'
+        )->orderBy('id', 'ASC')
+        ->get();
         return view('halloffame', [
             "results" => $results,
             "totals" => $totals
