04b4b5da8f0640705d7440a55a3910431f119a36
Kai Philipp <kphilipp@inquies.de>
2022-12-16 11:54:03 +0000

    removed home button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
-                    <b-nav-item href="/"><b-icon-house-fill></b-icon-house-fill>&nbsp;Home</b-nav-item>
+                    <!-- <b-nav-item href="/"><b-icon-house-fill></b-icon-house-fill>&nbsp;Home</b-nav-item> -->
                     @auth
                     <!-- revert 'u' permission in productive mode -->
                     @if(auth()->user()->role === 's') 
