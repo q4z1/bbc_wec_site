bc3ff3202043e0a29c5d6c01ca0e9a7f915f21ff
kauberdi <git@kauberdi.com>
2022-12-26 13:09:18 -0100

    implement/fix WEC ranking monthly filter

--- resources/js/components/RankingComponent.vue
+++ resources/js/components/RankingComponent.vue
@@ -11,7 +11,7 @@
                 spinner-variant="primary"
                 class="d-inline-block"
                 >
-                    <b-form-select :disabled="loading" v-model="year" @change="filter" :options="yearRange"></b-form-select>
+                    <b-form-select :disabled="loading||alltime" v-model="year" @change="filter" :options="yearRange"></b-form-select>
                 </b-overlay>
             </b-col>
             <b-col>
@@ -23,10 +23,37 @@
                 spinner-variant="primary"
                 class="d-inline-block"
                 >
-                    <b-form-select :disabled="loading" v-model="month" @change="filter" :options="monthRange"></b-form-select>
+                    <b-form-select :disabled="loading||alltime||allyear" v-model="month" @change="filter" :options="monthRange"></b-form-select>
                 </b-overlay>
             </b-col>
-            <b-col></b-col>
+            <b-col>
+                <b-overlay
+                :show="loading"
+                rounded
+                opacity="0.6"
+                spinner-small
+                spinner-variant="primary"
+                class="d-inline-block"
+                >
+                    <b-form-checkbox :disabled="loading||alltime" class="mt-2" @change="filter" v-model="allyear" switch>
+                        All-Year
+                    </b-form-checkbox>
+                </b-overlay>
+            </b-col>
+            <b-col>
+                <b-overlay
+                :show="loading"
+                rounded
+                opacity="0.6"
+                spinner-small
+                spinner-variant="primary"
+                class="d-inline-block"
+                >
+                    <b-form-checkbox :disabled="loading||allyear" class="mt-2" @change="filter" v-model="alltime" switch>
+                        All-Time
+                    </b-form-checkbox>
+                </b-overlay>
+            </b-col>
         </b-row>
 
         <b-table striped hover 
@@ -51,12 +78,13 @@
                 year: 0,
                 month: 0,
                 loading: false,
+                allyear: false,
+                alltime: false,
             }
         },
         computed: {
             yearRange: function(){
                 let years = []
-                years.push({value: '0', text: 'All-Time'})
                 let now = this.current_year
                 let past = 2012
                 for(let i=now;i>=past;i--){
@@ -66,15 +94,28 @@
             },
             monthRange: function(){
                 let months = []
+                let monthText = []
+                monthText[1] = "January"
+                monthText[2] = "February"
+                monthText[3] = "March"
+                monthText[4] = "April"
+                monthText[5] = "May"
+                monthText[6] = "June"
+                monthText[7] = "July"
+                monthText[8] = "August"
+                monthText[9] = "September"
+                monthText[10] = "October"
+                monthText[11] = "November"
+                monthText[12] = "December"
                 for(let i=1;i<=12;i++){
-                    months.push({value: i, text: i})
+                    months.push({value: i, text: monthText[i]})
                 }
                 return months
             },
         },
         mounted() {
             this.current_year = this.year = new Date().getFullYear()
-            this.current_month = new Date().getMonth() + 1
+            this.month = new Date().getMonth() + 1
             this.result = this.formatResult(this.stats)
         },
         methods:{
@@ -88,8 +129,8 @@
                         'position': i + 1,
                         // 'player_id': s.player.id,
                         'nickname': s.player.nickname,
-                        'score': s.score_year,
-                        'games': s.year.games
+                        'score': !this.alltime && !this.allyear ? s.score_month : s.score_year,
+                        'games': !this.alltime && !this.allyear ? s.month.games : s.year.games
                     })
                 }
                 return stats_formatted
@@ -100,7 +141,8 @@
             filter(){
                 this.loading = true
                 axios.post('/results/ranking', {
-                    year: this.year,
+                    year: !this.alltime ? $this.year : 0,
+                    month: !this.alltime && !this.allyear ? $this.month : 0
                 })
                 .then(response => {
                     if(response.data.success === true){
