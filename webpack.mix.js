8d12ed5c32870a6b477589e744e90b3c50ddd8a1
kauberdi <git@kauberdi.com>
2023-02-01 16:03:37 -0100

    add versioning/cache bursting

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .copy('resources/css/theme.*.css', 'public/css').vue();
+mix.version();
